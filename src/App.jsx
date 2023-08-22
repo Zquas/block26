ade70f3857df3c6c327dff8df2ad55222424419c
Jason Chen <114283388+Zquas@users.noreply.github.com>
2023-08-21 18:45:09 -0700

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import "./App.css";
+import { useState } from "react";
+import ContactList from "./components/ContactList";
+
+function App() {
+  const [selectedContactId, setSelectedContactId] = useState(null);
+
+  return (
+    <>
+      {selectedContactId ? (
+        <div>Selected Contact View</div>
+      ) : (
+        <ContactList />
+      )}
+    </>
+  );
+}
+export default App
